f51eba29e15f4f2f3866a00fc13aad3fc041155b
zhiqsyr <zhqisyr@163.com>
2017-04-18 14:19:10 +0800

    Initial commit

---
+++ src/main/java/org/zhiqsyr/mfmall/MfmallApplication.java
@@ -0,0 +1,17 @@
+package org.zhiqsyr.mfmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * @author dongbz
+ * @since 2017/3/31
+ */
+@EnableAutoConfiguration
+public class MfmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfmallApplication.class, args);
+    }
+
+}
